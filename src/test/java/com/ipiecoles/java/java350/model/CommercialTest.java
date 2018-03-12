04e5fdc93c3e02a3eb08eedf218fe31758f1f3e3
Remy-BESSOn <Remy-BESSOn@10.251.37.31>
2018-03-12 11:23:04 +0100

    ajout de tests unitaires

--- src/test/java/com/ipiecoles/java/java350/model/CommercialTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/CommercialTest.java
@@ -9,6 +9,33 @@ public class CommercialTest {
 	public void test_getPrimeAnnuellewithCANull (){
 		//GIVEN
 		Commercial commercial = new Commercial();
+		commercial.setCaAnnuel(null);
+		
+		//WHEN
+		Double prime = commercial.getPrimeAnnuelle();
+		
+		//THEN
+		Assertions.assertThat(prime).isEqualTo(500d);
+	}
+	
+	@Test
+	public void test_getPrimeAnnuellewithCA0 (){
+		//GIVEN
+		Commercial commercial = new Commercial();
+		commercial.setCaAnnuel(0d);
+		
+		//WHEN
+		Double prime = commercial.getPrimeAnnuelle();
+		
+		//THEN
+		Assertions.assertThat(prime).isEqualTo(500d);
+	}
+	
+	@Test
+	public void test_getPrimeAnnuellewithCA50000 (){
+		//GIVEN
+		Commercial commercial = new Commercial();
+		commercial.setCaAnnuel(10000d);
 		
 		//WHEN
 		Double prime = commercial.getPrimeAnnuelle();
